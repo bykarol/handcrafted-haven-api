f56b0b38c648b60089f2003b754a64e3065c61cf
David <davidp@ntestinc.com>
2024-05-18 13:57:20 +0200

    comenting searchParams()

--- src/app/ui/search.tsx
+++ src/app/ui/search.tsx
@@ -5,7 +5,7 @@ import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 //import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
-  const searchParams = useSearchParams();
+  //const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
 
@@ -34,7 +34,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
         //onChange={(e) => {
         // handleSearch(e.target.value);
         //}}
-        defaultValue={searchParams.get('query')?.toString()}
+        //defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute right-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
