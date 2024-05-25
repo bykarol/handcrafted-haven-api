0304556b5c2baa3679d3016dd282e258c305cb1f
David <davidp@ntestinc.com>
2024-05-25 12:59:49 +0200

    adating serch functionality

--- src/app/handcrafted-haven/page.tsx
+++ src/app/handcrafted-haven/page.tsx
@@ -1,13 +1,34 @@
 import ProductRecomendationsList from '../ui/product/productRecomendationsList';
+import ProductList from '../ui/product/productList';
 import { CategoryList } from '../ui/category/CategoryList';
 import { primaryFont, secondaryFont } from '../ui/fonts';
+import Search from '@/app/ui/search';
+import { Suspense } from 'react';
+import { useSearchParams } from 'next/navigation';
 
-export default function Page() {
+export default function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  const query = searchParams?.query || '';
+  const currentPage = Number(searchParams?.page) || 1;
+
+  //const totalPages = await fetchInvoicesPages(query);
+
   return (
-    <main className='mx-20'>
-      {/* <h1>Handcrafted page</h1> */}
+    <main className="mx-20">
+      {/* Search Results */}
+      <h2 className="text-xl py-5 font-bold">Search Results</h2>
+      <Suspense fallback={<div>Loading...</div>}>
+        <ProductList query={query} currentPage={currentPage} />
+      </Suspense>
+
       {/* Product Recomendations List section */}
-      <h2 className='text-xl py-5 font-bold'>Product recommendations</h2>
+      <h2 className="text-xl py-5 font-bold">Product recommendations</h2>
       <ProductRecomendationsList />
 
       {/* Category List section */}
