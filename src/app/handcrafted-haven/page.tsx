5b34cf223ae69d4686defab6dda47cb447e74695
natyboo5 <nparedes517@yahoo.com>
2024-05-29 23:30:20 -0400

    product displayed by artisan, featured artisan component and about us content added

--- src/app/handcrafted-haven/page.tsx
+++ src/app/handcrafted-haven/page.tsx
@@ -4,6 +4,7 @@ import { CategoryList } from '../ui/category/CategoryList';
 import { primaryFont, secondaryFont } from '../ui/fonts';
 import Search from '@/app/ui/search';
 import { Suspense } from 'react';
+import FeaturedArtisan from '../ui/artisan/FeaturedArtisanCard';
 
 export default function Page({
   searchParams,
@@ -33,6 +34,10 @@ export default function Page({
       {/* Product Recomendations List section */}
       <h2 className="text-xl py-5 font-bold">Product recommendations</h2>
       <ProductRecomendationsList />
+
+      <div>
+            <FeaturedArtisan />
+        </div>
 
       {/* Category List section */}
       <h2 className="text-xl py-5 font-bold"> Shop by Category</h2>
