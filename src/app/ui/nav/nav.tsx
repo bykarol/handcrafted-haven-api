8fce1debf5dd93f41cfc2cd4cecc1895f44b381b
bykarol <brachokarol.sud@gmail.com>
2024-06-05 11:05:23 +0200

    Revert "update Image component in nav.tsx"
    This reverts commit d2a0616ad90ec1e08e79aa0eafaa917d74800f82.

--- src/app/ui/nav/nav.tsx
+++ src/app/ui/nav/nav.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import NavLinks from '@/app/ui/nav/nav-links';
-import Image from 'next/image';
 import Search from '@/app/ui/search';
 import { primaryFont, secondaryFont } from '@/app/ui/fonts';
 import {
@@ -26,10 +25,6 @@ export default function Nav() {
       menuClases = ['hidden', 'md:flex'];
     }
     return menuClases.join(' ');
-  }
-  let profileImg = "/default-image.png";
-  if (session?.user?.image) {
-    profileImg = session.user.image;
   }
 
   function displayButton() {
@@ -77,11 +72,9 @@ export default function Nav() {
         {session?.user ? (
           <div className="Flex items-center">
             <p>{session.user.name}</p>
-            <Image
-              src={profileImg}
-              alt={`Photo of ${session.user.name}`}
-              width={40}
-              height={40}
+            <img
+              src={session.user.image ?? '/default-image.png'}
+              alt={session.user.name ?? 'User Image'}
               className="w-10 h-10 rounded-full center"
             />
 
